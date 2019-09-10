86b8ee8e49d22e09c6bcb49eb378d35d7f2e79c5
Eduardo Castaneda <eduardo.castaneda@g.austincc.edu>
2019-09-09 19:04:15 -0500

    Classwork 9/9/2019 Examples

--- test/examples_test/02_module_test/02_module_tests.cpp
+++ test/examples_test/02_module_test/02_module_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "if.h"
+#include "if_else.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -8,6 +9,16 @@ TEST_CASE("Verify Test Configuration", "verification") {
 
 TEST_CASE("Test is_even function")
 {
-	REQUIRE(is_even(2) == true)
-	REQUIRE(is_even(3) == false)
+	REQUIRE(is_even(2) == true);
+	REQUIRE(is_even(3) == false);
+}
+
+TEST_CASE("Test get generation")
+{
+	REQUIRE(get_generation(2000) == "Centenial");
+	REQUIRE(get_generation(1990) == "Millenial");
+	REQUIRE(get_generation(1970) == "Generation X");
+	REQUIRE(get_generation(1955) == "Baby Boomer");
+	REQUIRE(get_generation(1931) == "Silent Generation");
+	REQUIRE(get_generation(1776) == "Invalid Year");
 }
