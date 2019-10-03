01bafb67d61a9902deb9ea24d26dbba2dcdb3f6c
Eduardo Castaneda <eduardo.castaneda@g.austincc.edu>
2019-10-02 19:24:30 -0500

    10/2/2019 worked with rectangle class

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -1,14 +1,31 @@
 #include "bank_account.h"
 #include <iostream>
+#include <vector>
 
 using std::cout;
 using std::cin;
+using std::vector;
+
 
 int main()
 {
-	BankAccount account;
+	vector<BankAccount> accounts;
+	BankAccount b1;
+
+	accounts.push_back(b1);
+	accounts.push_back(BankAccount(750));
+
+	//accounts.push_back(BankAccount());
+	
+	for (auto account : accounts)
+	{
+		cout << "Balance: " << account.get_balance() << "\n" ;
+	}
+	
+	/*BankAccount account;
 	account.deposit(5);
 	cout << account.get_balance();
+	*/
 
 	return 0;
 }
